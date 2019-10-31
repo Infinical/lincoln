98e25339dfc0382da87976a0a9954ab93935637e
Infinical <inficalvin@gmail.com>
2019-10-31 06:20:47 +0300

    fix

--- src/app/pages/ambassador/create-project/create-project.component.ts
+++ src/app/pages/ambassador/create-project/create-project.component.ts
@@ -62,60 +62,60 @@ export class CreateProjectComponent implements OnInit {
     );
   }
 
-  uploadFile() {
-    var bar = document.getElementById('js-progressbar');
+  // uploadFile() {
+  //   var bar = document.getElementById('js-progressbar');
 
-    UIkit.upload('.js-upload', {
-      url: '',
-      multiple: true,
+  //   UIkit.upload('.js-upload', {
+  //     url: '',
+  //     multiple: true,
 
-      beforeSend: function() {
-        console.log('beforeSend', arguments);
-      },
-      beforeAll: function() {
-        console.log('beforeAll', arguments);
-      },
-      load: function() {
-        console.log('load', arguments);
-      },
-      error: function() {
-        console.log('error', arguments);
-      },
-      complete: function() {
-        console.log('complete', arguments);
-      },
+  //     beforeSend: function() {
+  //       console.log('beforeSend', arguments);
+  //     },
+  //     beforeAll: function() {
+  //       console.log('beforeAll', arguments);
+  //     },
+  //     load: function() {
+  //       console.log('load', arguments);
+  //     },
+  //     error: function() {
+  //       console.log('error', arguments);
+  //     },
+  //     complete: function() {
+  //       console.log('complete', arguments);
+  //     },
 
-      loadStart: function(e) {
-        console.log('loadStart', arguments);
+  //     loadStart: function(e) {
+  //       console.log('loadStart', arguments);
 
-        bar.removeAttribute('hidden');
-        bar.max = e.total;
-        bar.value = e.loaded;
-      },
+  //       bar.removeAttribute('hidden');
+  //       bar.max = e.total;
+  //       bar.value = e.loaded;
+  //     },
 
-      progress: function(e) {
-        console.log('progress', arguments);
+  //     progress: function(e) {
+  //       console.log('progress', arguments);
 
-        bar.max = e.total;
-        bar.value = e.loaded;
-      },
+  //       bar.max = e.total;
+  //       bar.value = e.loaded;
+  //     },
 
-      loadEnd: function(e) {
-        console.log('loadEnd', arguments);
+  //     loadEnd: function(e) {
+  //       console.log('loadEnd', arguments);
 
-        bar.max = e.total;
-        bar.value = e.loaded;
-      },
+  //       bar.max = e.total;
+  //       bar.value = e.loaded;
+  //     },
 
-      completeAll: function() {
-        console.log('completeAll', arguments);
+  //     completeAll: function() {
+  //       console.log('completeAll', arguments);
 
-        setTimeout(function() {
-          bar.setAttribute('hidden', 'hidden');
-        }, 1000);
+  //       setTimeout(function() {
+  //         bar.setAttribute('hidden', 'hidden');
+  //       }, 1000);
 
-        alert('Upload Completed');
-      }
-    });
-  }
+  //       alert('Upload Completed');
+  //     }
+  //   });
+  // }
 }
